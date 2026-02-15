93e0327347c5451f11d47143b25b9bddf504ddde
Henry Chen <99457027+Chenry513@users.noreply.github.com>
2026-02-14 17:56:11 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,14 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Build Vue.js frontend
+cd frontend
+npm install
+npm run build
+cd ..
+
 cd dl_tools
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py process_markdown
+
